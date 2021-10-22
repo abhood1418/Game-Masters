6bb39c8d23234346769d87cee1baea2aedaae462
Andrew Hood <andrewhood@Andrews-MacBook-Pro.local>
2021-10-22 17:19:35 -0400

    connected airtable

--- gamemasters/src/App.js
+++ gamemasters/src/App.js
@@ -2,15 +2,15 @@ import './App.css';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { Link, Route } from 'react-router-dom';
-import Header from 'src/components/Header/Header.js';
-import Details from './components/Details/Details.js';
-import Footer from 'src/components/Footer/Footer.js';
-import Comment from 'src/components/Comment/Comment.js';
-import Post from 'src/components/Post/Post.js';
-import Resources from 'src/components/Resources/Resources.js';
+// import Header from 'src/components/Header/Header.js';
+// import Details from './components/Details/Details.js';
+// import Footer from 'src/components/Footer/Footer.js';
+// import Comment from 'src/components/Comment/Comment.js';
+// import Post from 'src/components/Post/Post.js';
+// import Resources from 'src/components/Resources/Resources.js';
 
 //Post GET URL
-const API_URL = "https://api.airtable.com/v0/appwDf75VNBRVmPrC/Table%201?api_key=keyTC7ZYPD0132E0A";
+const API_URL = "https://api.airtable.com/v0/appwDf75VNBRVmPrC/Post?api_key=keyTC7ZYPD0132E0A";
 
 
 function App() {
@@ -19,11 +19,12 @@ function App() {
   useEffect(() => {
     const fetchPosts = async () => {
       const resp = await axios.get(API_URL);
-      setPostData(resp.data);
+      setPostData(resp.data.records);
+      console.log(postData)
     }
+    
     fetchPosts();
   }, [])
-  
   //Data Handling 
   const [postData, setPostData] = useState([]);
   const [commentData, setCommentData] = useState([]);
@@ -37,6 +38,11 @@ function App() {
   return (
     <div>
       <h2>Post Test</h2>
+        {postData.map((post, idx) => (
+          <Link key={idx} to={`/`}>
+            <p>{post.fields.title}</p>
+          </Link>
+        ))}
     </div>
   );
 }
