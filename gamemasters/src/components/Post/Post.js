c41655da3ceb54ee7af2b3650ea64d861889140e
Andrew Hood <andrewhood@Andrews-MacBook-Pro.local>
2021-10-24 08:25:12 -0400

    added post form

--- gamemasters/src/components/Post/Post.js
+++ gamemasters/src/components/Post/Post.js
@@ -2,8 +2,20 @@
 
 const Post = () => {
   return (
-    <div>
-      <h1>New Post</h1>
+    <div className="newpostdiv">
+      <form className="newpost">
+        <h1>Create A Post</h1>
+        <label htmlFor="title">Post Title: </label>
+        <input type="text" id="title" placeholder="title"></input>
+        <label htmlFor="tagline">Tagline: </label>
+        <input type="text" id="tagline" placeholder="tagline"></input>
+        <label htmlFor="image">Image URL: </label>
+        <input type="text" id="image" placeholder="image url"></input>
+        <label htmlFor="content">Content: </label>
+        <textarea type='text' id="content" placeholder="content..."></textarea>
+        <label htmlFor="author">Author: </label>
+        <input type="text" id="author" placeholder="author"></input>
+      </form>
     </div>
   )
 }
