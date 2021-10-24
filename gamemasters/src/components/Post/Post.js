955bad0f2a410bb2af5afb57f5652cc8ff4476f1
Andrew Hood <andrewhood@Andrews-MacBook-Pro.local>
2021-10-24 11:03:40 -0400

    style for Post

--- gamemasters/src/components/Post/Post.js
+++ gamemasters/src/components/Post/Post.js
@@ -3,6 +3,7 @@ import Button from '@mui/material/Button';
 import axios from 'axios';
 import { useState } from 'react';
 import { Redirect, useParams } from 'react-router-dom';
+import './Post.css';
 
 const API_URL = "https://api.airtable.com/v0/appwDf75VNBRVmPrC/Post?api_key=keyTC7ZYPD0132E0A";
 
@@ -45,16 +46,16 @@ const Post = ({setToggleFetch, toggleFetch}) => {
       <form className="newpost" onSubmit={handlePostRequest}>
         <h1>Create A Post</h1>
         <label htmlFor="title">Post Title: </label>
-        <input type="text" id="title" placeholder="title" onChange={(ev) => setTitle(ev.target.value)}></input>
+        <input className="inputs" type="text" id="title" placeholder="title" onChange={(ev) => setTitle(ev.target.value)}></input>
         <label htmlFor="tagline">Tagline: </label>
-        <input type="text" id="tagline" placeholder="tagline" onChange={(ev) => setDescription(ev.target.value)}></input>
+        <input className="inputs" type="text" id="tagline" placeholder="tagline" onChange={(ev) => setDescription(ev.target.value)}></input>
         <label htmlFor="image">Image URL: </label>
-        <input type="text" id="image" placeholder="image url" onChange={(ev) => setImg(ev.target.value)}></input>
+        <input className="inputs" type="text" id="image" placeholder="image url" onChange={(ev) => setImg(ev.target.value)}></input>
         <label htmlFor="content">Content: </label>
         <textarea type='text' id="content" placeholder="content..." onChange={(ev) => setComment(ev.target.value)}></textarea>
         <label htmlFor="author">Author: </label>
-        <input type="text" id="author" placeholder="author" onChange={(ev) => setAuthor(ev.target.value)}></input>
-        <Button variant="contained" type="submit">Submit</Button>
+        <input className="inputs" type="text" id="author" placeholder="author" onChange={(ev) => setAuthor(ev.target.value)}></input>
+        <Button variant="contained" type="submit" id="submitpost">Submit</Button>
       </form>
     </div>
   )
