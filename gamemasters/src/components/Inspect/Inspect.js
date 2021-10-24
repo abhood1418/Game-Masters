a9929f1446368a57b50a241b4bc618bff23c6e25
Andrew Hood <andrewhood@Andrews-MacBook-Pro.local>
2021-10-24 07:56:06 -0400

    set up inspect fields

--- gamemasters/src/components/Inspect/Inspect.js
+++ gamemasters/src/components/Inspect/Inspect.js
@@ -6,8 +6,12 @@ const Inspect = ({ postData }) => {
   const details = postData.find((detail) => detail.id === params.id);
   console.log(postData);
   return (
-    <div>
+    <div className='inspection'>
       <h1>{details.fields.title}</h1>
+      <h2>{details.fields.description}</h2>
+      <img src={details.fields.img}/>
+      <p>{details.fields.comment}</p>
+      <em>{details.fields.author}</em>
     </div>
   )
 }
