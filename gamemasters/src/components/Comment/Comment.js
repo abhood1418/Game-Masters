91d668f7b9367812cb29000d47ffbe51466152f0
Andrew Hood <andrewhood@Andrews-MacBook-Pro.local>
2021-10-25 14:06:19 -0400

    create comment component toggle

---
+++ gamemasters/src/components/Comment/Comment.js
@@ -0,0 +1,11 @@
+
+
+const Comment = ({ toggleComment, setToggleComment }) => {
+  return (
+    <div>
+      <h2>comment space</h2>
+    </div> 
+  )
+}
+
+export default Comment;
--- gamemasters/src/components/Inspect/Inspect.js
+++ gamemasters/src/components/Inspect/Inspect.js
@@ -1,19 +1,40 @@
 import { useParams } from "react-router-dom";
+import { useState } from 'react';
 import AddCommentIcon from '@mui/icons-material/AddComment';
+import Comment from '../Comment/Comment.js';
 import './Inspect.css';
 
 
 const Inspect = ({ postData }) => {
+  const [toggleComment, setToggleComment] = useState(false);
   const params = useParams();
   const details = postData.find((detail) => detail.id === params.id);
   console.log(postData);
+
+  const showComment = (ev) => {
+    setToggleComment(!toggleComment);
+  }
+
+
   return (
     <div className='inspection'>
       <h1>{details.fields.title}</h1>
       <h2>{details.fields.description}</h2>
       <img src={details.fields.img} className="inspimg"/>
-      <p className="describe">{details.fields.comment}<br/><br/><em className="author"><strong>{details.fields.author}</strong></em><br/><br/><button className="combtn"><AddCommentIcon /></button></p>
-      {/* <button className="combtn"><AddCommentIcon /></button> */}
+      <p className="describe">
+        {details.fields.comment}
+        <br />
+        <br />
+        <em className="author">
+          <strong>{details.fields.author}</strong>
+        </em>
+        <br />
+        <br />
+        <button className="combtn" onClick={showComment}>
+          <AddCommentIcon />
+        </button>
+      </p>
+      {toggleComment ? <Comment /> : null}
     </div>
   )
 }
