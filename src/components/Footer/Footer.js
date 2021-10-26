bb04f3801cb5b506b651c7b99f68c39fd67ab785
Andrew Hood <andrewhood@Andrews-MacBook-Pro.local>
2021-10-26 17:51:23 -0400

    added media queries

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Github from './Github.png';
 import Linkedin from './Linkedin.png';
 import './Footer.css';
@@ -10,6 +11,9 @@ const Footer = () => {
       <img src={Github} alt="github" className="footimg" />
       <img src={Linkedin} alt="linkedin" className="footimg" />
       <h6>Created By: ABHood - 2021</h6>
+      <Link>
+      <p>Egg?</p>
+      </Link>
     </div>
   )
 }
