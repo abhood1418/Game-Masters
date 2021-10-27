564ff2935b70c932c4598b496db6a1de4c6e50fe
Andrew Hood <andrewhood@Andrews-MacBook-Pro.local>
2021-10-27 10:49:55 -0400

    attached links to footer imgs

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,8 +8,12 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div className="footer">
+      <a href="https://github.com/abhood1418">
       <img src={Github} alt="github" className="footimg" />
+      </a>
+      <a href="https://www.linkedin.com/in/andrew-hood-29408b165/">
       <img src={Linkedin} alt="linkedin" className="footimg" />
+      </a>
       <h6>Created By: ABHood - 2021</h6>
       <Link to="/egg">
       <p>Egg?</p>
